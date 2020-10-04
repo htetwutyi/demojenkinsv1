182dbc4cc9189dc0a3c452d200f5ef90d6d67e7d
htetwutyi <47908167+htetwutyi@users.noreply.github.com>
2020-10-04 22:00:13 +0630

    To test Jenkins and Tomcat

---
+++ src/main/java/com/jenkins/demojenkinsv1/Demojenkinsv1Application.java
@@ -0,0 +1,13 @@
+package com.jenkins.demojenkinsv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demojenkinsv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demojenkinsv1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/demojenkinsv1/Demojenkinsv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.demojenkinsv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demojenkinsv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
